057cd3681866803c3d9bb7bdcdc029261f0fdb32
SPY4ER <mirza_376@hotmail.com>
2018-03-12 21:30:29 +0100

    breakthrough

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
-const request = require ("request");
+const request = require('request');
+
+
+var adr = "ustikolina";
+var encoded = encodeURIComponent(adr);
 
 request({
-url: "http://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%2020%20philadelphia",
-xml: true
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encoded}`,
+  json: true
 }, (error, response, body) => {
-console.log(body);
 
+if (error) {
+console.log("Unable to connect to google service");
+}
+else if (body.status == "OVER_QUERY_LIMIT") {
+console.log("Unable to connect to google service");
+}
 
-}  );
+console.log(JSON.stringify(body, undefined, 2));
+
+console.log("--------------------------------------");
+
+  console.log(`Address: ${body.results[0].formatted_address}`);
+  //console.log(adr); 
+  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+  console.log(`Longitude: ${body.results[0].geometry.location.lng}`); 
+});
